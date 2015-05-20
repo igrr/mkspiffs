5c960883ff5446ef82d0439367128877c9e0a8f3
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-05-20 17:19:29 +0300

    Add page and block size arguments

--- main.cpp
+++ main.cpp
@@ -21,20 +21,22 @@
 #include "tclap/CmdLine.h"
 #include "tclap/UnlabeledValueArg.h"
 
-#define LOG_PAGE_SIZE       256
-
 static std::vector<uint8_t> s_flashmem;
+
 static std::string s_dirName;
 static std::string s_imageName;
 static int s_imageSize;
+static int s_pageSize;
+static int s_blockSize;
 
-enum Action { ACTION_NONE, ACTION_PACK, ACTION_LIST };
+enum Action { ACTION_NONE, ACTION_PACK, ACTION_LIST, ACTION_VISUALIZE };
 static Action s_action = ACTION_NONE;
 
-static spiffs _filesystemStorageHandle;
-static u8_t spiffs_work_buf[LOG_PAGE_SIZE*2];
-static u8_t spiffs_fds[32*4];
-static u8_t spiffs_cache[(LOG_PAGE_SIZE+32)*4];
+static spiffs s_fs;
+
+static std::vector<uint8_t> s_spiffsWorkBuf;
+static std::vector<uint8_t> s_spiffsFds;
+static std::vector<uint8_t> s_spiffsCache;
 
 
 static s32_t api_spiffs_read(u32_t addr, u32_t size, u8_t *dst)
@@ -55,69 +57,63 @@ static s32_t api_spiffs_erase(u32_t addr, u32_t size)
     return SPIFFS_OK;
 } 
 
-spiffs_config spiffs_get_storage_config()
+void checkCallback(spiffs_check_type type, spiffs_check_report report,
+                                   u32_t arg1, u32_t arg2)
+{
+}
+
+void spiffsMount(bool init)
 {
     spiffs_config cfg = {0};
     
     cfg.phys_addr = 0x0000;
     cfg.phys_size = (u32_t) s_flashmem.size();
-    
-    const int flash_sector_size = 4096;
-    const int log_page_size = 256;
-    
-    cfg.phys_erase_block = flash_sector_size; // according to datasheet
-    cfg.log_block_size = flash_sector_size * 2; // Important to make large
-    cfg.log_page_size = log_page_size; // as we said
-    return cfg;
-}
-
-void check_callback(spiffs_check_type type, spiffs_check_report report,
-                                   u32_t arg1, u32_t arg2)
-{
-}
-
-void spiffs_mount(bool init)
-{
-    spiffs_config cfg = spiffs_get_storage_config();
-    
-//    debugf("fs.start:%x, size:%d Kb\n", cfg.phys_addr, cfg.phys_size / 1024);
+        
+    cfg.phys_erase_block = s_blockSize;
+    cfg.log_block_size = s_blockSize;
+    cfg.log_page_size = s_pageSize;
     
     cfg.hal_read_f = api_spiffs_read;
     cfg.hal_write_f = api_spiffs_write;
     cfg.hal_erase_f = api_spiffs_erase;
     
-    int res = SPIFFS_mount(&_filesystemStorageHandle,
+    const int maxOpenFiles = 4;
+    s_spiffsWorkBuf.resize(s_pageSize * 2);
+    s_spiffsFds.resize(32 * maxOpenFiles);
+    s_spiffsCache.resize((32 + s_pageSize) * maxOpenFiles);
+
+
+    int res = SPIFFS_mount(&s_fs,
                            &cfg,
-                           spiffs_work_buf,
-                           spiffs_fds,
-                           sizeof(spiffs_fds),
-                           spiffs_cache,
-                           sizeof(spiffs_cache),
+                           &s_spiffsWorkBuf[0],
+                           &s_spiffsFds[0],
+                           s_spiffsFds.size(),
+                           &s_spiffsCache[0],
+                           s_spiffsCache.size(),
                            NULL);
     
 //    debugf("mount res: %d\n", res);
     if (init) {
-        spiffs_file fd = SPIFFS_open(&_filesystemStorageHandle, "tmp", SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
+        spiffs_file fd = SPIFFS_open(&s_fs, "tmp", SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
         uint8_t tmp = 0xff;
-        SPIFFS_write(&_filesystemStorageHandle, fd, &tmp, 1);
-        SPIFFS_fremove(&_filesystemStorageHandle, fd);
-        SPIFFS_close(&_filesystemStorageHandle, fd);
+        SPIFFS_write(&s_fs, fd, &tmp, 1);
+        SPIFFS_fremove(&s_fs, fd);
+        SPIFFS_close(&s_fs, fd);
     } else {
-        _filesystemStorageHandle.check_cb_f = &check_callback;
-        //SPIFFS_check(&_filesystemStorageHandle);
+        s_fs.check_cb_f = &checkCallback;
+        //SPIFFS_check(&s_fs);
     }
 }
 
-void spiffs_unmount()
+void spiffsUnmount()
 {
     uint32_t total, used;
-    SPIFFS_info(&_filesystemStorageHandle, &total, &used);
-    SPIFFS_vis(&_filesystemStorageHandle);
+    SPIFFS_info(&s_fs, &total, &used);
     std::cerr << "total: " << total << ", used: " << used << std::endl;
-    SPIFFS_unmount(&_filesystemStorageHandle);
+    SPIFFS_unmount(&s_fs);
 }
 
-int add_file(char* name, const char* path) {
+int addFile(char* name, const char* path) {
     const size_t write_size = 512;
     uint8_t write_block[write_size];
     
@@ -127,7 +123,7 @@ int add_file(char* name, const char* path) {
         return 1;
     }
     
-    spiffs_file dst = SPIFFS_open(&_filesystemStorageHandle, name, SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
+    spiffs_file dst = SPIFFS_open(&s_fs, name, SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR, 0);
     
     fseek(src, 0, SEEK_END);
     size_t size = ftell(src);
@@ -138,24 +134,29 @@ int add_file(char* name, const char* path) {
     {
         size_t will_write = std::min(left, write_size);
         if (will_write != fread(write_block, 1, will_write, src)) {
-            std::cerr << "error: failed to read from file";
             fclose(src);
-            SPIFFS_close(&_filesystemStorageHandle, dst);
+            SPIFFS_close(&s_fs, dst);
             return 1;
         }
-        SPIFFS_write(&_filesystemStorageHandle, dst, write_block, write_size);
+        int res = SPIFFS_write(&s_fs, dst, write_block, write_size);
+        if (res < 0) {
+            fclose(src);
+            SPIFFS_close(&s_fs, dst);
+            return 1;
+        }
         left -= will_write;
     }
     
-    SPIFFS_close(&_filesystemStorageHandle, dst);
+    SPIFFS_close(&s_fs, dst);
     fclose(src);
     return 0;
 }
 
-int add_files(const char* dirname)
+int addFiles(const char* dirname)
 {
     DIR *dir;
     struct dirent *ent;
+    bool error = false;
     if ((dir = opendir (dirname)) != NULL) {
         while ((ent = readdir (dir)) != NULL) {
             if (ent->d_name[0] == '.')
@@ -170,24 +171,27 @@ int add_files(const char* dirname)
                 continue;
             }
             
-            std::cerr << "adding " << ent->d_name << std::endl;
-            if (add_file(ent->d_name, fullpath.c_str()) != 0) {
+            std::cerr << "adding " << ent->d_name;
+            if (addFile(ent->d_name, fullpath.c_str()) != 0) {
+                std::cerr << " - error!" << std::endl;
+                error = true;
                 break;
             }
+            std::cerr << std::endl;
         }
         closedir (dir);
     } else {
         std::cerr << "warning: can't read source directory" << std::endl;
         return 1;
     }
-    return 0;
+    return (error) ? 1 : 0;
 }
 
 void listFiles() {
     spiffs_DIR dir;
     spiffs_dirent ent;
     
-    spiffs_DIR* res = SPIFFS_opendir(&_filesystemStorageHandle, 0, &dir);
+    spiffs_DIR* res = SPIFFS_opendir(&s_fs, 0, &dir);
     spiffs_dirent* it;
     while (true) {
         it = SPIFFS_readdir(&dir, &ent);
@@ -210,16 +214,14 @@ int actionPack() {
         return 1;
     }
     
-    spiffs_mount(true);
-    add_files(s_dirName.c_str());
-    spiffs_unmount();
-    
+    spiffsMount(true);
+    int result = addFiles(s_dirName.c_str());
+    spiffsUnmount();
     
     fwrite(&s_flashmem[0], 4, s_flashmem.size()/4, fdres);
-    
     fclose(fdres);
     
-    return 0;
+    return result;
 }
 
 
@@ -235,23 +237,48 @@ int actionList() {
     fread(&s_flashmem[0], 4, s_flashmem.size()/4, fdsrc);
     fclose(fdsrc);
     
-    spiffs_mount(false);
+    spiffsMount(false);
     listFiles();
-    spiffs_unmount();
+    spiffsUnmount();
     
     return 0;
 }
 
-int processArgs(int argc, const char** argv) {
+int actionVisualize() {
+    s_flashmem.resize(s_imageSize, 0xff);
+    
+    FILE* fdsrc = fopen(s_imageName.c_str(), "rb");
+    if (!fdsrc) {
+        std::cerr << "error: failed to open image file" << std::endl;
+        return 1;
+    }
+    
+    fread(&s_flashmem[0], 4, s_flashmem.size()/4, fdsrc);
+    fclose(fdsrc);
+    
+    spiffsMount(false);
+    SPIFFS_vis(&s_fs);
+    spiffsUnmount();
+    
+    return 0;
+}
+
+void processArgs(int argc, const char** argv) {
     
     TCLAP::CmdLine cmd("", ' ', "0.1");
     TCLAP::ValueArg<std::string> packArg( "c", "create", "create spiffs image from a directory", true, "", "pack_dir");
-    TCLAP::SwitchArg listArg( "l", "list", "list spiffs image to a directory", false);
+    TCLAP::SwitchArg listArg( "l", "list", "list files in spiffs image", false);
+    TCLAP::SwitchArg visualizeArg( "i", "visualize", "visualize spiffs image", false);
     TCLAP::UnlabeledValueArg<std::string> outNameArg( "image_file", "spiffs image file", true, "", "image_file"  );
-    TCLAP::ValueArg<int> sizeArg( "s", "fs_size", "fs image size, in bytes", false, 0xb000, "number" );
+    TCLAP::ValueArg<int> imageSizeArg( "s", "size", "fs image size, in bytes", false, 0x10000, "number" );
+    TCLAP::ValueArg<int> pageSizeArg( "p", "page", "fs page size, in bytes", false, 256, "number" );
+    TCLAP::ValueArg<int> blockSizeArg( "b", "block", "fs block size, in bytes", false, 4096, "number" );
 
-    cmd.add( sizeArg );
-    cmd.xorAdd( packArg, listArg );
+    cmd.add( imageSizeArg );
+    cmd.add( pageSizeArg );
+    cmd.add( blockSizeArg );
+    std::vector<TCLAP::Arg*> args = {&packArg, &listArg, &visualizeArg};
+    cmd.xorAdd( args );
     cmd.add( outNameArg );
     cmd.parse( argc, argv );
     
@@ -262,11 +289,14 @@ int processArgs(int argc, const char** argv) {
     else if (listArg.isSet()) {
         s_action = ACTION_LIST;
     }
+    else if (visualizeArg.isSet()) {
+        s_action = ACTION_VISUALIZE;
+    }
     
     s_imageName = outNameArg.getValue();
-    s_imageSize = sizeArg.getValue();
-    
-    return 0;
+    s_imageSize = imageSizeArg.getValue();
+    s_pageSize  = pageSizeArg.getValue();
+    s_blockSize = blockSizeArg.getValue();
 }
 
 int main(int argc, const char * argv[]) {
@@ -283,6 +313,7 @@ int main(int argc, const char * argv[]) {
     switch (s_action) {
         case ACTION_PACK: return actionPack();
         case ACTION_LIST: return actionList();
+        case ACTION_VISUALIZE: return actionVisualize();
         default: ;
     }
     
