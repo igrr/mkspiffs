9e6b39a9b6f95fa99946972ea92064d46fce7043
kroegerama <1519044+kroegerama@users.noreply.github.com>
2018-03-04 23:57:23 +0100

    Update build_all_configs.sh
    `SPIFFS_OBJ_META_LEN` was not properly set for all languages (only for c).
    See output of `mkspiffs --version`.

--- build_all_configs.sh
+++ build_all_configs.sh
@@ -16,12 +16,12 @@ make dist BUILD_CONFIG_NAME="-arduino-esp8266" \
 # Build configuration for arduino-esp32
 make clean
 make dist BUILD_CONFIG_NAME="-arduino-esp32" \
-    CFLAGS="-DSPIFFS_OBJ_META_LEN=4"
+    CPPFLAGS="-DSPIFFS_OBJ_META_LEN=4"
 
 # Build configuration for ESP-IDF (esp32)
 make clean
 make dist BUILD_CONFIG_NAME="-esp-idf" \
-    CFLAGS="-DSPIFFS_OBJ_META_LEN=4"
+    CPPFLAGS="-DSPIFFS_OBJ_META_LEN=4"
 
 
 
